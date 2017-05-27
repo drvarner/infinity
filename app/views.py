13ebe05d8811b77e4603aff17e7d233bf09262e6
David Varner <david.r.varner@gmail.com>
2017-05-27 19:28:43 -0400

    Add view to read army code

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from flask import render_template
+import lzstring
+
+from flask import render_template, request
 
 from app import app
 from app.models import Faction
@@ -14,3 +16,34 @@ def index():
 def faction(army_id):
     faction = Faction.query.filter_by(army_id=army_id).one()
     return render_template('faction.html', faction=faction)
+
+
+@app.route('/armyCode/<code>', methods=['GET', 'POST'])
+def armyCode(code):
+    decoded = lzstring.LZString().decompressFromBase64(code).split('|')
+
+    faction = Faction.query.filter_by(army_id=decoded[0]).one()
+    groups = breakGroups(decoded[3])
+    units = []
+
+    for group in groups:
+        units.append(group.split('!'))
+
+    units.pop()
+    units.pop()
+
+    armyList = {
+        'Title': decoded[2],
+        'Points': decoded[1],
+        'Units': units
+    }
+
+    return render_template(
+        'code_info.html',
+        armyList=armyList,
+        faction=faction
+    )
+
+
+def breakGroups(code, splitstring='@'):
+    return code.split(splitstring)
